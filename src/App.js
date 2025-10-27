cab98c240569e48e23099f050c5a5dfba806028c
HoCHoRoo <rttkhc0304@gmail.com>
2025-10-27 20:13:49 +0900

    feat: App.js.에 게임 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { InputCarName, InputTryCount } from "./io/input";
+import RacingGame from "./game/RacingGame";
+
 class App {
-  async run() {}
+  async run() {
+    const carNames = await InputCarName();
+    const rounds = await InputTryCount();
+
+    const game = new RacingGame(carNames, rounds);
+    await game.play();
+  }
 }
 
 export default App;
